1ee44d477761cf803043e64f2594e8f0d0d609f3
gregbubnis <gbubnis@gmail.com>
2023-09-09 10:28:49 -0700

    download grid inside polygon perimeter

--- decimate/getdata.py
+++ decimate/getdata.py
@@ -9,6 +9,7 @@ import xml.etree.ElementTree as ET
 from pandas import json_normalize
 import numpy as np
 import pandas as pd
+import shapely.geometry
 
 
 def parse_kml(f):
@@ -24,6 +25,7 @@ def parse_kml(f):
     tree = ET.parse(f)
     txt = tree.find('.//{http://www.opengis.net/kml/2.2}coordinates').text
     xyz = np.vstack([[np.asarray([float(k) for k in st.split(',')])] for st in txt.split()])
+    xyz[:, 0:2] = xyz[:, [1, 0]]
     return xyz
 
 def get_elevation_post_opentopodata(data=None):
@@ -46,44 +48,44 @@ def get_elevation_post_opentopodata(data=None):
         return None
 
 
-def get_area(corners=None, stride=50):
+def get_area(perimeter=None, stride=50):
     """download an area/grid from the opentopodata api
 
     POST requests are chunked to 100 points apiece and spaced 1.1s apart and
     can be interrupted/resumed if the partial csv files are not deleted
 
     Args:
-        corners (list): list of two (lat,lon) tuples for rectangle corners
-        stride (float): data spacing (in meters)
+        perimeter (list): list of two or more (lat, lon) tuples. If two points,
+          interpret as rectangle corners, otherwise, if more than two points,
+          interpert as a (sorted and closed) polygon perimeter.
+        stride (float): grid spacing (in meters)
     Returns:
         data (DataFrame): `x`, `y` columns are lon and lat in meters (elevation
-            is also in meters)
-
-    - stride is in meters
-    - returns a DataFrame
+          is also in meters)
     """
     os.makedirs('csv_chunks', exist_ok=True)
 
-    # build the grid
-    corners = np.asarray(corners)
-    lon = [np.min(corners[:, 1]), np.max(corners[:, 1])]
-    lat = [np.min(corners[:, 0]), np.max(corners[:, 0])]
-    lon_scale = np.cos(np.mean(lat)/180*np.pi)
-    x_stride = stride/111319./lon_scale
+    # determine lat/lon grid points to download
+    perimeter = np.asarray(perimeter)
+    lon_limits = [np.min(perimeter[:, 1]), np.max(perimeter[:, 1])]
+    lat_limits = [np.min(perimeter[:, 0]), np.max(perimeter[:, 0])]
+    lon_scale = np.abs(np.cos(np.mean(lat_limits)/180*np.pi))
+    x_stride = stride/111319./lon_scale # adjust longitude stride
     y_stride = stride/111319.
-    lonx = np.arange(*lon, x_stride)
-    laty = np.arange(*lat, y_stride)
+    lonx = np.arange(*lon_limits, x_stride)
+    laty = np.arange(*lat_limits, y_stride)
     latlon = list(itertools.product(laty, lonx))
+    if len(perimeter) > 2:
+        polygon = shapely.geometry.Polygon(perimeter)
+        latlon = [x for x in latlon if shapely.geometry.Point(x).within(polygon)]
 
-    # chunk the requests and then reassemble 
+    # download in chunks
     chunksize = 100
     tag = hashlib.md5((str(latlon)).encode()).hexdigest()[:8]
     df_todo = pd.DataFrame(data=latlon, columns=['lat', 'lon'])
-    df_todo['status'] = 0
     df_todo['chunk'] = np.arange(len(df_todo)) // chunksize
     all_csv = []
-
-    print('REQUESTING %i elevation points' % len(df_todo))
+    print('REQUESTING %i elevation points (%i chunks)' % (len(df_todo), len(df_todo)//chunksize+1))
     for i in df_todo['chunk'].unique():
         csv = 'csv_chunks/data_%s_%4.4i.csv' % (tag, i)      
         all_csv.append(csv)
@@ -111,7 +113,7 @@ if __name__ == "__main__":
 
     # download halfdome data at 50m resolution
     corners = [(37.7500, -119.5430), (37.7390, -119.5280)]
-    data = get_area(corners=corners, stride=50)
+    data = get_area(perimeter=corners, stride=50)
     #data.to_csv('data_halfdomecrop_srtm30m_0030m.csv', float_format='%8.4f')
 
 
